bed65e9e4a64c0f0c0b6ded0b5d2f8e935f46fb6
dlaperishvili <davitlaperishvili@gmail.com>
2023-01-04 23:44:43 +0400

    default structure added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+function App() {
+    return (
+        <>
+            <ToastContainer autoClose={1000} />
+            <div className="App">Welcome to the React Dictionary App</div>
+        </>
+    );
+}
+
+export default App;
